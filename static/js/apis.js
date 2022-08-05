272435cc0edf1df7accc5a8979614e00eb4b9499
alirezaisavand <isavand.alireza@gmail.com>
2022-08-05 23:08:00 +0430

    remove apis.js from base.html

--- static/js/apis.js
+++ static/js/apis.js
@@ -20,66 +20,66 @@ $("#loginbtn").on("click", function(){
         handleResponse("/videos/list", response, function ({token}) { window.localStorage.setItem('ACCESS_TOKEN', token) }, "نام کاربری یا رمز عبور اشتباه است");
     });
 });
-
-$("#registerbtn").on("click", function(){
-    var user = $("#username").val();
-    var password = $("#password").val();
-    var firstname = $("#firstname").val();
-    var lastname = $("#lastname").val();
-
-    $.post("/api/user/register", {username: user, password: password, firstname, lastname}, function (response) {
-        handleResponse("/videos/list", response, function ({token}) { window.localStorage.setItem('ACCESS_TOKEN', token) });
-    });
-});
-
-$("#logoutbtn").on("click", function(){
-    $.post("/api/user/logout", {action: 'logout'}, function (response) {
-    });
-});
-
-$("#likeBtn").on("click", function(){
-    var token = window.localStorage.getItem('ACCESS_TOKEN');
-    $.ajax({
-        url: "/video/felan/like",
-        headers: {"Authorization": "Token "+token},
-        data: {
-            // body
-        },
-        type: "POST",
-        success: function() {
-            // felan
-        }
-    });
-
-});
-
-$("#unlikeBtn").on("click", function(){
-    // felan
-});
-
-$("#loginRedirectBtn").on("click", function(){
-    window.location.href = "/login";
-});
-
-$("#registerRedirectBtn").on("click", function(){
-    window.location.href = "/register";
-});
-
-$("#new_ticket_button").on("click", function(){
-    var message = $("#ticket_message").val();
-
-    $.post("/api/tickets/new", {ticket_message: message}, function (response) {
-        handleResponse("/videos/list", response, function(r){}, "نام کاربری یا رمز عبور اشتباه است");
-    });
-});
-
-$("#cancel_button").on("click", function(){
-//    window.location.href = prev_url;
-});
-
-$("#add_message_button").on("click", function(){
-    var message = $("new_ticket_message").val();
-    $.post("/api/tickets/add_message", {ticket_id: {{ticket._id}}, username: {{user.username}}, ticket_message:message}, function (response) {
-        handleResponse('/videos/list', response, function (r) {}, 'مشکلی در اضافه کردن پیام پیش آمد.')
-    });
-});
+//
+//$("#registerbtn").on("click", function(){
+//    var user = $("#username").val();
+//    var password = $("#password").val();
+//    var firstname = $("#firstname").val();
+//    var lastname = $("#lastname").val();
+//
+//    $.post("/api/user/register", {username: user, password: password, firstname, lastname}, function (response) {
+//        handleResponse("/videos/list", response, function ({token}) { window.localStorage.setItem('ACCESS_TOKEN', token) });
+//    });
+//});
+//
+//$("#logoutbtn").on("click", function(){
+//    $.post("/api/user/logout", {action: 'logout'}, function (response) {
+//    });
+//});
+//
+//$("#likeBtn").on("click", function(){
+//    var token = window.localStorage.getItem('ACCESS_TOKEN');
+//    $.ajax({
+//        url: "/video/felan/like",
+//        headers: {"Authorization": "Token "+token},
+//        data: {
+//            // body
+//        },
+//        type: "POST",
+//        success: function() {
+//            // felan
+//        }
+//    });
+//
+//});
+//
+//$("#unlikeBtn").on("click", function(){
+//    // felan
+//});
+//
+//$("#loginRedirectBtn").on("click", function(){
+//    window.location.href = "/login";
+//});
+//
+//$("#registerRedirectBtn").on("click", function(){
+//    window.location.href = "/register";
+//});
+//
+//$("#new_ticket_button").on("click", function(){
+//    var message = $("#ticket_message").val();
+//
+//    $.post("/api/tickets/new", {ticket_message: message}, function (response) {
+//        handleResponse("/videos/list", response, function(r){}, "نام کاربری یا رمز عبور اشتباه است");
+//    });
+//});
+//
+//$("#cancel_button").on("click", function(){
+////    window.location.href = prev_url;
+//});
+//
+//$("#add_message_button").on("click", function(){
+//    var message = $("new_ticket_message").val();
+//    $.post("/api/tickets/add_message", {ticket_id: {{ticket._id}}, username: {{user.username}}, ticket_message:message}, function (response) {
+//        handleResponse('/videos/list', response, function (r) {}, 'مشکلی در اضافه کردن پیام پیش آمد.')
+//    });
+//});
