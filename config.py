02af58b1d955b0eabeb5308bf4a0d44777ae7b97
Mikaeel <mikaeel.ghorbani@tapsi.cab>
2022-08-07 00:20:29 +0430

    update STAFF_IP

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ MONGO_URL = "mongodb://localhost:27017/networkDB"
 Videos_Folder = "static/uploaded_videos/"
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'DUIFEUIFGG(@E#DEUHUIEEH(*y3yfew9h'
 STAFF_IP = '23.94.98.201'
+GIT_SECRET = "IOEFIOIOvoifndoicn3r9023je"
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask
 from config import MONGO_URL, SECRET_KEY, STAFF_IP
 from controller import add_routes
 from pymodm import connect
+import git
 
 from service.user import UserService
 
@@ -14,4 +15,15 @@ if __name__ == "__main__":
     connect(MONGO_URL)
     UserService.register_admin()
     add_routes(app)
+
+
+    @app.route('/git_update', methods=['POST'])
+    def git_update():
+        repo = git.Repo('.')
+        origin = repo.remotes.origin
+        repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+        origin.pull()
+        return '', 200
+
+
     app.run(debug=True, host="0.0.0.0")
